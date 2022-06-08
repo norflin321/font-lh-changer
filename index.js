cb64d476a5fbb65f21f602041518cf3b2c6d6437
Anton Kurtin <norflin321@gmail.com>
2022-06-08 03:59:03 +0300

    fixes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const opentype = require('opentype.js');
 
-const FONT_FILE = "./font.otf";
-const INCREASE_BY = 500;
+const FONT_FILE = "./font.ttf";
+const CHANGE_BY = 500;
 
 (async () => {
   try {
     const font = await opentype.load(FONT_FILE);
-    font.ascender += INCREASE_BY;
-    font.tables["os2"].sTypoAscender += INCREASE_BY;
-    font.tables["hhea"].ascender += INCREASE_BY;
-    font.descender -= INCREASE_BY;
-    font.tables["os2"].sTypoDescender -= INCREASE_BY;
-    font.tables["hhea"].descender -= INCREASE_BY;
+    font.ascender += CHANGE_BY;
+    font.tables["os2"].sTypoAscender += CHANGE_BY;
+    font.tables["hhea"].ascender += CHANGE_BY;
+    font.descender -= CHANGE_BY;
+    font.tables["os2"].sTypoDescender -= CHANGE_BY;
+    font.tables["hhea"].descender -= CHANGE_BY;
     const arr = FONT_FILE.split(".");
-    arr[arr.length - 2] += `-lh-increased-by-${INCREASE_BY}`;
+    arr[arr.length - 2] += `_lh_changed_by_${CHANGE_BY}`;
     font.download(arr.join("."));
   } catch (err) {
     console.error(err);
